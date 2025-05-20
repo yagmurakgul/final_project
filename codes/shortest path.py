31950e70b3db4d16b0905f24cabebd315c5c182a
Yağmur Akgül <yagmurakgul@Yagmurs-MacBook-Air-3.local>
2025-05-21 00:34:40 +0300

    İlk yükleme

---
+++ codes/shortest path.py
@@ -0,0 +1,32 @@
+import heapq
+
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}  # Başlangıçta tüm mesafeler sonsuz
+    distances[start] = 0
+    pq = [(0, start)]  # (mesafe, düğüm)
+
+    while pq:
+        current_distance, current_node = heapq.heappop(pq)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+
+    return distances
+
+
+if __name__ == "__main__":
+    graph = {
+        'A': {'B': 1, 'C': 4},
+        'B': {'A': 1, 'C': 2, 'D': 5},
+        'C': {'A': 4, 'B': 2, 'D': 1},
+        'D': {'B': 5, 'C': 1}
+    }
+
+    print(dijkstra(graph, 'A'))
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. shortestpath documentation master file, created by
+   sphinx-quickstart on Tue May 20 01:15:48 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+shortestpath documentation
+==========================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
