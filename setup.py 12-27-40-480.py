8326348d862f49cf8196834d8fa7799c40185ee2
Yağmur Akgül <yagmurakgul@Yagmurs-MacBook-Air-3.local>
2025-06-03 14:52:29 +0300

    Apply .gitignore and clean tracked files

---
+++ setup.py 12-27-40-480.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sp211_2220674004_44',
+    version='0.1.0',
+    author='Yağmur Akgül',
+    author_email='yagmur@example.com',
+    description='Shortest path calculation using Dijkstra algorithm',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yagmurakgul/final_project',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
